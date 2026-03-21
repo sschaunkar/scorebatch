bd8901e04547afd40d12f828b3f4205c70e1d93c
SinghSachin <sachin.singh@wheebox.com>
2026-03-21 23:42:39 +0530

    Initial commit

---
+++ src/main/java/com/ets/scorebatch/ScorebatchApplication.java
@@ -0,0 +1,13 @@
+package com.ets.scorebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorebatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ets/scorebatch/ScorebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ets.scorebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
