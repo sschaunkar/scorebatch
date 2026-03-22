60ac51dbc4219a6cfc9026da7ff95160c88f7feb
SinghSachin <sachin.singh@wheebox.com>
2026-03-22 13:56:11 +0530

    stopping a job

--- src/main/java/com/ets/scorebatch/controller/JobController.java
+++ src/main/java/com/ets/scorebatch/controller/JobController.java
@@ -2,6 +2,7 @@ package com.ets.scorebatch.controller;
 
 import java.util.List;
 
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +20,23 @@ public class JobController {
 	@Autowired
 	JobService jobservice;
 	
+	@Autowired
+	JobOperator joboperator;
+	
 	@PostMapping("/start/{jobName}")
 	public String startJob(@PathVariable String jobName, @RequestBody List<JobRequestParams> paramslist) {
 		jobservice.startJob(jobName,paramslist);
 		return jobName+" started";
+	}
+	
+	@PostMapping("/stop/{jobExecutionId}")
+	public String startJob(@PathVariable long jobExecutionId) {
+		try {
+			joboperator.stop(jobExecutionId);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "Job Stopped";
+		
 	}
 }
